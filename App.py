be75669492e27be1e6568350ae1d8090426c7650
lucas_hub12 <hyeonggyunbae@gmail.com>
2025-02-05 12:20:05 +0900

    한방에 인서트

--- App.py
+++ App.py
@@ -28,7 +28,17 @@ DB_CONFIG = {
 def get_connection():
     return psycopg.connect(**DB_CONFIG)
 
-st.title("팀순신 점심 기록장")
+def insert_menu(menu_name, member_name, dt):
+          conn = get_connection()
+          cursor = conn.cursor()
+          cursor.execute(
+                  "INSERT INTO lunch_menu (menu_name, member_name, dt) VALUES (%s,%s,%s);",
+                  (menu_name, member_name, dt)
+          )
+          conn.commit()
+          cursor.close()
+          conn.close()
+
 st.subheader("입력")
 menu_name = st.text_input("메뉴 이름", placeholder="예: 김치찌개")
 member_name = st.text_input("먹은 사람", value = "Lucas")
@@ -38,15 +48,8 @@ isPress = st.button("메뉴 저장")
 
 if isPress:
     if menu_name and member_name and dt:
-        conn = get_connection()
-        cursor = conn.cursor()
-        cursor.execute(
-                "INSERT INTO lunch_menu (menu_name, member_name, dt) VALUES (%s,%s,%s);",
-                (menu_name, member_name, dt)
-        )   
-        conn.commit()
-        cursor.close()
-        st.success(f"{isPress}:{menu_name},{member_name},{dt}")
+        insert_menu(menu_name, member_name, dt)
+        st.success("입력 성공")
     else:
         st.warning(f"모든 값을 입력해주세요!")
 
@@ -61,7 +64,7 @@ rows = cursor.fetchall()
 
 # conn.commit() -> 여기서는 자동으로 됨
 cursor.close()
-
+conn.close()
 
 # selected_df = pd.DataFrame([[1,2,3],[4,5,6]], columns=['a','b','c'])
 selected_df = pd.DataFrame(rows, columns=['menu_name', 'member_name','dt'])
@@ -93,25 +96,6 @@ gdf
 # https://docs.streamlit.io/develop/api-reference/charts/st.pyplot
 fig, ax = plt.subplots()
 gdf.plot(x="member_name", y="menu_name", kind="bar", ax=ax)
-st.pyplot(fig)
-
-st.subheader("CSV 통계")
-df = pd.read_csv('note/lunch_menu.csv')
-
-start_idx = df.columns.get_loc('2025-01-07')
-melted_df = df.melt(id_vars=['ename'], value_vars=df.columns[start_idx:-2], 
-                     var_name='dt', value_name='menu')
-
-not_na_df = melted_df[~melted_df['menu'].isin(['-','x','<결석>'])]
-gdf = not_na_df.groupby('ename')['menu'].count().reset_index()
-#gdf.plot(x="ename", y="menu", kind="bar")
-
-gdf
-
-# 📊 Matplotlib로 바 차트 그리기
-# https://docs.streamlit.io/develop/api-reference/charts/st.pyplot
-fig, ax = plt.subplots()
-gdf.plot(x="ename", y="menu", kind="bar", ax=ax)
 st.pyplot(fig)
 
 # TO DO
