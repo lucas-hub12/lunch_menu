12247ab6d905f7e5c18c0f93f830f80ac12259b3
lucas_hub12 <hyeonggyunbae@gmail.com>
2025-02-11 11:11:38 +0900

    나이계산기 완료

--- pages/8_age_cal.py
+++ pages/8_age_cal.py
@@ -1,9 +1,22 @@
 import streamlit as st
+import requests
+import lunch_menu.constants as const
+import datetime
+
 st.set_page_config(page_title="API", page_icon="📅")
 st.markdown("# 📅 API")
 st.sidebar.header("나이계산기")
 
-dt = st.date_input("생일 입력")
-if st.button("메뉴 저장"):
-    # TODO API 호출 - 받은 값 처리
-    st.success(f"나이 계산기{dt}")
+dt = st.date_input("생일 입력", value="today", min_value=datetime.date(1900,1,1))
+if st.button("조회 하기"):
+    headers = {
+        'accept': 'application/json'
+    }   
+    r = requests.get(f'{const.API_AGE}/{dt}', headers=headers)
+    if r.status_code == 200:
+        # TODO age를 받아오는 부분을 만들어주세요.
+        data = r.json()
+        age = data['age']
+        st.success(f"{dt} 일생의 나이는 {age}입니다.")
+    else:
+        st.error(f"문제가 발생했습니다. 관리자 문의:{r.status_code}")
